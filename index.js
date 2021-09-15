c0c6c315fe875db3fd4785edf53b325f8a55a59d
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2021-09-15 01:52:24 +0000

    split events to files

--- index.js
+++ index.js
@@ -11,37 +11,16 @@ const client = new Client({
   ]
 });
 
-client.commands = new Collection()
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
-}
-
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return
-
-  const command = client.commands.get(interaction.commandName)
-  if (!command) return
-
-  try {
-    await command.execute(interaction)
-  } catch (err) {
-    console.error(err)
-    await interaction.reply({
-      content: 'There was an error while executing the command!',
-      ephemeral: true,
-    })
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`)
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args))
+  } else {
+    client.on(event.name, (...args) => event.execute(...args))
   }
-});
-
+}
 
 // You really don't want your token here since your repl's code
 // is publically available. We'll take advantage of a Repl.it 
