adafd8617ec5fef0c9551c7fcc0e86cee44476cd
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2021-09-15 14:58:07 +0000

    create club function

--- msg_commands/createClub.js
+++ msg_commands/createClub.js
@@ -1,7 +1,46 @@
+const { Permissions } = require('discord.js');
+
 module.exports = {
     name: 'create',
+    description: `\`\`\`
+    Command: c?create
+    Description: Create a club with specified name.
+    Format: c?create [clubName]\`\`\`
+    `,
     
     async execute(msg, ...args) {
-        msg.reply(`c?create: ${args}`)
+        const clubName = args.join('-')
+        if (!clubName) {
+            msg.channel.send(this.description)
+            return
+        }
+        
+        const presRole = await msg.guild.roles.create({
+            name: `${clubName}-president`,
+        }).catch(err => {
+            msg.channel.send('Club with specified name is already created')
+            console.log(err)
+            return
+        })
+        
+        const channel = await msg.guild.channels.create(clubName, {
+            type: 'GUILD_TEXT',
+            permissionOverwrites: [
+                {
+                    id: msg.guild.id,
+                    deny: [Permissions.FLAGS.VIEW_CHANNEL]
+                },
+                {
+                    id: presRole.id,
+                    allow: [Permissions.FLAGS.VIEW_CHANNEL]
+                }
+            ]
+        }).catch(err => {
+            msg.channel.send('Failed to create club')
+            console.log(err)
+            return
+        })
+
+        await msg.channel.send(`Club created`)
     }
 }
