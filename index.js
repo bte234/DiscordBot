58954fa1616afae050ac18c8fabdbc9d2f9d4900
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2021-09-15 01:33:44 +0000

    command setup

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
+const fs = require('fs')
 // ================= START BOT CODE ===================
-const {Client, Intents} = require('discord.js');
+const {Client, Collection, Intents} = require('discord.js');
 
 const intents = new Intents(268437552)
 const client = new Client({
@@ -18,15 +11,38 @@ const client = new Client({
   ]
 });
 
-client.on('ready', () => {
+client.commands = new Collection()
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong!');
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return
+
+  const command = client.commands.get(interaction.commandName)
+  if (!command) return
+
+  try {
+    await command.execute(interaction)
+  } catch (err) {
+    console.error(err)
+    await interaction.reply({
+      content: 'There was an error while executing the command!',
+      ephemeral: true,
+    })
   }
 });
+
+
 // You really don't want your token here since your repl's code
 // is publically available. We'll take advantage of a Repl.it 
 // feature to hide the token we got earlier. 
