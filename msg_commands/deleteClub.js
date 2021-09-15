c7f196452fb20bd7178b46557fa0d25e3bf031b6
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2021-09-15 17:13:49 +0000

    delete club

--- msg_commands/deleteClub.js
+++ msg_commands/deleteClub.js
@@ -1,7 +1,34 @@
+const { Permissions } = require('discord.js');
+
 module.exports = {
     name: 'delete',
+    description: `\`\`\`
+    Command: c?delete
+    Description: Delete a club
+    Format: c?delete [clubName]\`\`\`
+    `,
     
     async execute(msg, ...args) {
-        msg.reply(`c?delete: ${args}`)
+        if (!msg.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+            return
+        }
+
+        const clubName = args.join('-')
+        if (!clubName) {
+            msg.channel.send(this.description)
+            return
+        }
+
+        const channel = msg.guild.channels.cache.find(r => r.name === clubName)
+        if (!channel) {
+            msg.channel.send(`Found no club with name ${clubName}`)
+            return
+        }
+
+        const presRole = msg.guild.roles.cache.find(r => r.name === `${clubName}-president`)
+        channel.delete()
+        presRole.delete()
+
+        msg.channel.send(`Deleted club ${clubName}`)
     }
 }
