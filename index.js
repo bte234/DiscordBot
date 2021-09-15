5eb172fafbe54a25e65cecee60b26cb2c106d9ff
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2021-09-15 00:47:00 +0000

    initial bot setup

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello world!'))
-app.listen(port, () => console.log(`app is listening at port ${port}`))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+// ================= START BOT CODE ===================
+const {Client, Intents} = require('discord.js');
+
+const intents = new Intents(268437552)
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ]
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong!');
+  }
+});
+// You really don't want your token here since your repl's code
+// is publically available. We'll take advantage of a Repl.it 
+// feature to hide the token we got earlier. 
+client.login(process.env.DISCORD_TOKEN);
